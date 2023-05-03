382fbaf926267ed5fce0197d9fa6360a9b3c85e0
Kulkarni-Vaishnavi <srivaishnavi2605@gmail.com>
2023-05-03 17:22:09 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,32 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Card } from '../components/Card'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Card } from "../components/Card";
+import axios from "axios";
 
 const Container = styled.div`
-    display: flex;
-    justify-content : space-between;
-    flex-wrap : wrap;    
+  display : flex;
+  justify-content: space-between;
+  flex-wrap : wrap;
 `;
 
-export const Home = () => {
+export const Home = ({type}) => {
+
+  //fetching videos from backend
+  const [videos,setVideos] = useState([]);
+
+  useEffect(()=> {
+    const fetchVideos = async ()=> {
+      const res = await axios.get(`http://localhost:8800/api/videos/${type}`);
+      setVideos(res.data.videos);
+    }
+    fetchVideos();
+  },[type]);//type is arguement -- props
+
   return (
     <Container>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-
+        {videos.map((singleVideo) => (
+          <Card key={singleVideo._id} video={singleVideo} />
+        ))}
     </Container>
-  )
-}
+  );
+};
